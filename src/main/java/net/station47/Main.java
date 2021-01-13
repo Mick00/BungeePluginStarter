210801c3d848d67b918d9983c3db64cc0c3d0559
Michael Duchesne <20918417+Mick00@users.noreply.github.com>
2021-01-12 21:20:08 -0500

    init

---
+++ src/main/java/net/station47/Main.java
@@ -0,0 +1,14 @@
+package net.station47;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class Main extends Plugin {
+
+    @Override
+    public void onEnable(){
+        getLogger().info("Hello world");
+    }
+
+
+
+}
